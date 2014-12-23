66b33dacfcaaa613153d563acb1883461e6f5b1e
Tyler Mitchell <zastari@gmail.com>
2014-12-23 08:16:55 +0000

    Removed defaults in configspec for required config params; removed required_keys dict from GlobalConfig methods

--- masterha_toolkit/conf/config.py
+++ masterha_toolkit/conf/config.py
@@ -4,8 +4,7 @@ from validate import Validator
 
 class GlobalConfig(object):
 
-    def _parse_config(self, config_file, config_spec,
-                      required_keys, validator):
+    def _parse_config(self, config_file, config_spec, validator):
         if validator == None:
             val = Validator()
         else:
@@ -15,25 +14,15 @@ class GlobalConfig(object):
         else:
             config = ConfigObj(None, configspec=config_spec)
         if config_spec is not None:
-            if not config.validate(val):
-		return 1
-	    for key in required_keys:
-		if key not in config.dict():
-		    return 1
-		elif required_keys[key] not in config[key]:
-		    return 1
+            config.validate(val)
         return config.dict()
 
-    def _add_config(self, config_file, config_spec=None,
-                   required_keys=None, validator=None):
-        new_config = self._parse_config(config_file, config_spec,
-                                        required_keys, validator)
+    def _add_config(self, config_file, config_spec=None, validator=None):
+        new_config = self._parse_config(config_file, config_spec, validator)
         self.config.update(new_config)
 
-    def _add_dispatch(self, dispatch_method, config_spec=None,
-                      required_keys=None, validator=None):
-        config_file = os.path.join(self.config_dir, 'dispatch',
-                                   dispatch_method) + ".cnf"
+    def _add_dispatch(self, dispatch_method, config_spec=None, validator=None):
+        config_file = os.path.join(self.config_dir, 'dispatch', dispatch_method) + ".cnf"
         self._add_config(config_file)
         
     def __init__(self, config_file):
@@ -42,11 +31,10 @@ class GlobalConfig(object):
         self.config_dir = os.path.dirname(config_file)
 	config_spec = """
 	[dispatcher]
-	dispatch_method         = string(default=None)
+	dispatch_method         = string
 	mha_cluster_conf        = string(default=None)
 	report_email            = string(default=None)
 	""".splitlines()
-	required_keys = { "dispatcher" : "dispatch_method" }
         
-        self._add_config(config_file, config_spec, required_keys)
+        self._add_config(config_file, config_spec)
         self._add_dispatch(self.config["dispatcher"]["dispatch_method"])
