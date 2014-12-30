25b574863d33a14e38134cdbf7e508b74dab8482
Tyler Mitchell <zastari@gmail.com>
2014-12-30 11:51:07 +0000

    Increment minor version number

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ test_requirements = [
 
 setup(
     name='masterha_toolkit',
-    version='0.1.0',
+    version='0.2.0',
     description='Tools for MySQL MasterHA failover and monitoring',
     long_description=readme + '\n\n' + history,
     author='Tyler Mitchell',
